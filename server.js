d586b2dddb8197618278606d6fb9e188e5dd6f4d
Sidney San Martín <s@sidneysm.com>
2012-11-09 22:27:52 -0500

    Even better/hackier way to include the client JS

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var http = require('http');
 var express = require('express');
 var fs = require('fs');
-// Hax
-var clientJS = __dirname + '/node_modules/webrtc.io-client/lib/webrtc.io.js';
 var app = express();
 
 app.use(express.static(__dirname + '/static'));
-app.get('/webrtc.io.js', function(req, res){ res.sendfile(clientJS); });
 
 app.listen(8000);
