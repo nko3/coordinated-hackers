122dda51b55fc26888ee24bc312696fa730794fc
Sam Epstein <same008@gmail.com>
2012-11-11 16:44:43 -0500

    Parallelize GIF creation with WebWorkers

--- static/js/gif.js
+++ static/js/gif.js
@@ -2,26 +2,58 @@
 
 var gif = (function(){
 	var exports = {};
-	var encoder = new GIFEncoder();
-	exports.encoder = encoder;
-
 	var canvas = document.createElement("canvas");
 	var ctx = canvas.getContext("2d");
 
-	encoder.setRepeat(0);
-	encoder.setDelay(300);
-	encoder.setQuality(4);
-	encoder.start();
+	exports.canvas = canvas;
+	exports.ctx = ctx;
 
 	var screenshotButton = document.getElementById("screenshot");
 	var finishButton = document.getElementById("finishGif");
 	var gifContainer = document.getElementById("gifContainer");
+	var canvasDataArray = [];
+	var workerArray = [
+		new Worker("js/animWorker.js"),
+		new Worker("js/animWorker.js"),
+		new Worker("js/animWorker.js"),
+		new Worker("js/animWorker.js")
+	];
 
 	screenshotButton.addEventListener("click", screenShot);
 	finishButton.addEventListener("click", finishGif);
 	interval.addEventListener("input", function(){
-		encoder.setDelay(interval.value);
+		delay = interval.value;
 	});
+
+	var delay = 100;
+	var animation_parts = [];
+
+	for (var worker in workerArray) {
+
+		workerArray[worker].onmessage = function(e)
+		{
+			//handle stuff, like get the frame_index
+			var frame_index = e.data["frame_index"];
+			var frame_data = e.data["frame_data"];
+			animation_parts[frame_index] = frame_data;
+			for(var j = 0; j < canvasDataArray.length; j++)
+			{
+				if(animation_parts[j] == null)
+				{
+					return;
+				}
+			}
+			//check when everything else is done and then do animation_parts.join('') and have fun
+			var binary_gif = animation_parts.join('');
+			var data_url = 'data:image/gif;base64,'+window.btoa(binary_gif);
+
+			var gifItem = new Image();
+			gifItem.src = data_url;
+			document.getElementById("gifContainer").appendChild(gifItem);
+			canvasDataArray = [];
+			animation_parts = [];
+		}
+	}
 
 	function screenShot() {
 
@@ -31,16 +63,17 @@ var gif = (function(){
         },1000);
 
 		Array.prototype.forEach.call(document.getElementById('videos').children, function(video) {
-			ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-			encoder.addFrame(ctx);
+			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+			canvasDataArray.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
 		});
 	}
 	function finishGif() {
-		encoder.finish();
-		var gifOutput = document.createElement("img");
-		gifOutput.src = "data:image/gif;base64,"+encode64(encoder.stream().getData());
-		gifContainer.appendChild(gifOutput);
-		encoder.start();
+		var workerIndex = 0;
+		for (var i in canvasDataArray) {
+			if (workerIndex >= workerArray.length) workerIndex = 0;
+			workerArray[workerIndex].postMessage({"frame_index": +i, "delay": delay, "frame_length":canvasDataArray.length-1, "height":canvas.height, "width":canvas.width, "imageData":canvasDataArray[i].data}); //imarray.join(',')
+			workerIndex++;
+		}
 	}
 	canvas.width = 640;
 	canvas.height = 480;
